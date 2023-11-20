e710fd887b867fa4dbe0da8f26e3b2a4471f5f76
JeetJhaveri <jeetwork941@gmail.com>
2023-11-20 17:41:09 +0530

    check user api is been integrated

--- MedFarmApi/Features/User/UserController.cs
+++ MedFarmApi/Features/User/UserController.cs
@@ -29,7 +29,7 @@ namespace MadFarmApi.Features.User
         public async Task<IActionResult> GetAll()
         {
             var user = await databaseContext.Users.ToListAsync();
-            if(user is null) return NotFound();
+            if (user is null) return NotFound();
             return Ok(user);
         }
 
@@ -37,8 +37,31 @@ namespace MadFarmApi.Features.User
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
             var user = await databaseContext.Users.FirstOrDefaultAsync(x => x.Id == id);
-            if(user is null) return NotFound();
+            if (user is null) return NotFound();
             return Ok(user);
+        }
+
+        [HttpGet("/CheckUser")]
+        public async Task<IActionResult> CheckUser([FromBody] User checkUser)
+        {
+            var user = await databaseContext.Users.Where(user => user.Email == checkUser.Email && user.Password == checkUser.Password).ToListAsync();
+            if (user is null) return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser([FromBody] User newUser, [FromRoute] int id)
+        {
+            var user = await databaseContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if(user is null) return NotFound();
+
+            user.Name = newUser.Name;
+            user.Email = newUser.Email;
+            user.Password = newUser.Password;
+            user.Address = newUser.Address;
+            await databaseContext.SaveChangesAsync();
+
+            return Ok("User Updated !");
         }
     }
 }
---
+++ MedFarmApi/wwwroot/uploads/About.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <div className="container-fluid about pt-5">
+        <div className="container">
+          <div className="row gx-5">
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <div className="d-flex border border-5 border-primary">
+                <img
+                  className="mt-auto mx-auto w-100"
+                  src="img/about.jpg"
+                  alt="about"
+                />
+              </div>
+              <div className="row gx-5 gy-4 mt-4">
+                <div className="col-sm-6">
+                  <i className="fa fa-seedling display-1 text-secondary" />
+                  <h4>100% Organic</h4>
+                  <p className="mb-0">
+                    Labore justo vero ipsum sit clita erat lorem magna clita
+                    nonumy dolor magna dolor vero
+                  </p>
+                </div>
+                <div className="col-sm-6">
+                  <i className="fa fa-award display-1 text-secondary" />
+                  <h4>Award Winning</h4>
+                  <p className="mb-0">
+                    Labore justo vero ipsum sit clita erat lorem magna clita
+                    nonumy dolor magna dolor vero
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 pb-5">
+              <div className="mb-3 pb-2">
+                <h1 className="display-5">
+                  Marvel Agro & Dairy Farm <br />
+                </h1>
+                <h4>
+                  "Banshi Farm", Lakhabaval, Nr. Shivalik Shine Party Plot
+                </h4>
+              </div>
+                <h6 className="text-primary text-uppercase">History :</h6>
+              <p className="mb-4">
+                The foundation was laid down in 2021 with intension to provide
+                pure and natural products at a door steps. Scarcity of 100% pure
+                Gir Cow milk & Organic Products nearby area was initiation of
+                foundation stone. MARWELL AGRO & DAIRY FARM is non-NGO type
+                partnership Firm. Firm is ZERO CARBON EMMISSION with 100%
+                recycle renewal energy-based production line. We are using solar
+                roof top, own cultivation of green fodder using waste generation
+                of herds. Pesticides & "JIVAMRUT" Phosphate based fertilizers.
+              </p>
+              <h6 className="text-primary text-uppercase">
+                Industry Experience :
+              </h6>
+              <p className="mb-4">
+                4 partners have diversified experience in their own fields but
+                none of them having relevant experience and that's the beauty of
+                establishment of the firm. "40+" - experience of all individual
+                partners has been converted into "4T+" (Trust, Transparency,
+                Target & Taste ). We have transformed this reality into four
+                pillars of our firm.
+              </p>
+              <h6 className="text-primary text-uppercase">Capacity :</h6>
+              <p className="mb-4">
+                Current population of 25 herds with an average milk production
+                of 60 Ltr/Day since inception is now targeting to doubling the
+                capacity to reach 100 Ltr/Day in near future. Expansion will not
+                only increase the customer base but will also enhance the feed
+                for other unique products under development at site.
+              </p>
+              <h6 className="text-primary text-uppercase">Product Range :</h6>
+              <p className="mb-4">
+                ● 100% pure GIR COW milk <br /> ● Vermicompose Fertilizer, Wash
+                Water, JIVAMRUT <br />● Cow Dung Cakes & Gau Mutra <br />● Idols
+                made from cow dung & incense sticks <br />● Organic Vegetables &
+                Fruits <br />● Butter Milk & Ghee - product line without using
+                any mechanical device to get the pure taste with flavor of
+                well-established Indian Style.
+              </p>
+              <h6 className="text-primary text-uppercase">Our Mission :</h6>
+              <p className="mb-4">
+                "To Deliver Best from Soil to Plate" To improve customer need
+                and lifestyle by unlocking the nature gift and potential of
+                partner's dream, we are confident to achieve the company goal on
+                an exponential growth.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
